7ee969e8817586b6de4f4058c0c49a0f73300036
Chim Non <leconglong@gmail.com>
2015-02-27 23:41:52 +0700

    zzz

--- ps/js/functions.js
+++ ps/js/functions.js
@@ -115,11 +115,11 @@ jQuery(function($) {
 			}
 		});
 
-		// $('a[name=submit-mixy-form').bind('click touchstart',function(){
-		// 	if ($('form[name=mixy-form]').valid()) {
-		// 		$('form[name=mixy-form]').submit();
-		// 	}
-		// });
+		$('a[name=submit-mixy-form').bind('click tap',function(){
+			if ($('form[name=mixy-form]').valid()) {
+				$('form[name=mixy-form]').submit();
+			}
+		});
     });
 
 	function submitMixyForm() {
